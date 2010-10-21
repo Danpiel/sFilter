33c1c090dccf0c1a3fcab25d624f049d5a0939d1
danpiel <danpiel@gmail.com>
2010-10-21 17:28:54 +0300

    + Added Shaman, Priest, Hunter, Druid spells + Added missing spells for Warlock, Paladin, Rogue (thx to Malkom), Mage + Attempt to fix for cnTW clients + TOC. update

--- sFilter.lua
+++ sFilter.lua
@@ -33,12 +33,16 @@ local function sFilter_CreateFrame(data)
                     self.found = true
                     self.icon:SetTexture(icon)
                     self.count:SetText(count>1 and count or "")
+					if (duration) then
                     if(duration>=0) then
                         self.cooldown:Show()
                         CooldownFrame_SetTimer(self.cooldown, expirationTime-duration, duration, 1)
                     else
                         self.cooldown:Hide()
                     end
+				else 
+				self.cooldown:Hide()
+				end
                     break
                 end
             end
